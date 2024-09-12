49a44e6895bf91fa40f7b287e968245e10a06a1c
Victor Marques <marques.vct@hotmail.com>
2024-09-12 11:20:01 -0300

    chore: add initial configs and shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+import "./globals.css";
+
+export function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button>Olá Mundo</Button>
+    </div>
+  );
+}
