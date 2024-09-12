4abea4a66aed0f295f4129aee291a39a96169208
Victor Marques <marques.vct@hotmail.com>
2024-09-12 15:10:04 -0300

    feat: add sign up page

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
+import { SignUp } from "./pages/auth/sign-up";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,9 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <AuthLayout />,
-    children: [{ path: "/sign-in", element: <SignIn /> }],
+    children: [
+      { path: "/sign-in", element: <SignIn /> },
+      { path: "/sign-up", element: <SignUp /> },
+    ],
   },
 ]);
