eb43d1ebb00535383e900a7810c096f373a37042
Victor Marques <marques.vct@hotmail.com>
2024-09-12 18:42:09 -0300

    feat: add shadcn dark mode provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import "./globals.css";
 import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
