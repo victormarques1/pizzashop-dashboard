e6c1185d6883edda77931376f902fbfeb0b18afb
Victor Marques <marques.vct@hotmail.com>
2024-09-18 00:19:12 -0300

    feat: add error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,30 +1,46 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { Dashboard } from "./pages/app/dashboard/dashboard";
-import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
-import { SignUp } from "./pages/auth/sign-up";
-import { Orders } from "./pages/app/orders/orders";
 import { NotFound } from "./pages/404";
+import { Dashboard } from "./pages/app/dashboard/dashboard";
+import { Orders } from "./pages/app/orders/orders";
+import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
+import { Error } from "./pages/error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
-      { path: "/", element: <Dashboard /> },
-      { path: "/orders", element: <Orders /> },
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
+      },
     ],
   },
-
   {
     path: "/",
     element: <AuthLayout />,
     children: [
-      { path: "/sign-in", element: <SignIn /> },
-      { path: "/sign-up", element: <SignUp /> },
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
     ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
